75f3a2f199659597710b84a42239b9153c1f3b16
amrmousa95 <amrmousab95@gmail.com>
2020-03-24 00:29:53 +0400

    edit price in extra small screen size

--- js/main.js
+++ js/main.js
@@ -1,19 +1,5 @@
 $(function(){
-    "use strict";
-    // nice scroll
-  $("html").niceScroll();
-
-    window.onscroll = function() {myFunction()};
-
-    var header = document.getElementById("header");
-    var sticky = header.offsetTop;
-    
-    function myFunction() {
-      if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-      } else {
-        header.classList.remove("sticky");
-      }
-    }
-
-});
+   "use strict";
+   $("html").niceScroll();
+   
+})
