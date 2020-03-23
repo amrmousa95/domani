1651774315609badbda309ef1f64fd186bf589cf
amrmousa95 <amrmousab95@gmail.com>
2020-03-23 21:51:14 +0400

    first one

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+    "use strict";
+    // nice scroll
+    $("html").niceScroll();
+
+    window.onscroll = function() {myFunction()};
+
+    var header = document.getElementById("header");
+    var sticky = header.offsetTop;
+    
+    function myFunction() {
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    }
+
+});
